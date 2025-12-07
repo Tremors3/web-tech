b0156b501d50c0457243d62c45afeaa65479fa5f
Tremors3 <matte.menoz@gmail.com>
2025-12-07 13:49:47 +0100

    Upload auction's ListView in the home page

--- graba/apps/core/urls.py
+++ graba/apps/core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from core import views
+from .views import HomePageView
 
 app_name = 'core'
 
 urlpatterns = [
-    path(r'', views.home, name='home')
+    path(r'', HomePageView.as_view(), name='home')
 ]
--- graba/apps/core/views.py
+++ graba/apps/core/views.py
@@ -1,5 +1,40 @@
+from django.views.generic import ListView
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
+from django.utils.http import urlencode
+from django.db.models import Q
+from auctions.models import Auction
 
 
-def home(request):
-    return render(request, template_name='core/index.html', context={})
+class HomePageView(ListView):
+    model = Auction
+    template_name = 'core/index.html'
+    context_object_name = 'auctions'
+    paginate_by = 15
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        q = self.request.GET.get('q', '')
+        c = self.request.GET.get('c', '')
+
+        if q:
+            queryset = queryset.filter(title__icontains=q) | \
+            queryset.filter(description__icontains=q)
+
+        # if c:
+        #     queryset = queryset.filter(category=c)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+
+        # Copy GET and remove 'page'
+        params = self.request.GET.copy()
+        params._mutable = True
+        params.pop('page', None)
+
+        # Querystring clear for pagination
+        context['querystring'] = params.urlencode()
+
+        return context
